0aec5e7265b21bf5f19bb42daf4f5f895fe8320c
realchoky <realchoky@gmail.com>
2024-06-21 10:36:10 +0900

    123

--- javascript/animation/animation.js
+++ javascript/animation/animation.js
@@ -1,13 +1,21 @@
 let frameCount = 0;
+let ddgTop = 250;
 
 $(function() {
     imageAnimate();
+    ddgAnimate();
 });
 
 const imageAnimate = function() {
     const timer = setInterval(function() {
-        console.log(`images/${frameCount%6+1}.png`);
-        $("#walker").attr("src", `images/${frameCount%6+1}.png`);
-        frameCount++;        
-    },50);
+        $("#walker").attr("src", `images/${frameCount++%6+1}.png`);
+    },300);
+};
+
+const ddgAnimate = function() {
+    const timer = setInterval(function() {
+        if (ddgTop==100) ddgTop = 250;
+        console.log(`${ddgTop-=5}px`);
+        $("#ddg").css("top", `${ddgTop-=5}px`);        
+    },100);
 };
