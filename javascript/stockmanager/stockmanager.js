eea2cfe453cbdaa3a5280f7cd965d903b527d864
realchoky <realchoky@gmail.com>
2024-06-28 16:25:09 +0900

    123

--- javascript/stockmanager/stockmanager.js
+++ javascript/stockmanager/stockmanager.js
@@ -34,7 +34,7 @@ const initLocalStorage = () => {
 // 매장등록
 const writeShop = () => {
     const shopArr = getShopList();
-    shopArr.push(new Shop(getNextShopSeq(), $('#shname').val(), 0));
+    shopArr.unshift(new Shop(getNextShopSeq(), $('#shname').val(), 0));
     localStorage.setItem('shopList', JSON.stringify(shopArr));
     printShopList();
 };
@@ -48,27 +48,42 @@ const getNextShopSeq = () => {
 
 // 매장목록
 const getShopList = () => {
-    return JSON.parse(localStorage.getItem('shopList'));
+    return JSON.parse(localStorage.getItem('shopList'))
+            .sort((a, b) => b.shno - a.shno);
 };
 
 // 매장목록 출력
 const printShopList = () => {
     $('#shoplist table tbody').html('');
     getShopList().forEach(shop => {
-        let tr = $('<tr></tr>');
-        tr.append($('<td>' + shop.shno + '</td>'));
-        tr.append($('<td>' + shop.shname + '</td>'));
-        tr.append($('<td>' + shop.shtotst + '</td>'));
-        tr.append($('<td><input type="button" value="수정" /></td>'));
-        tr.append($('<td><input id="deleteShop' + shop.shno + '" type="button" value="삭제" /></td>'));
+        let tr = $("<tr></tr>");
+        tr.append($("<td>"+shop.shno+"</td>"));
+        tr.append($("<td><input id='shname"+shop.shno+"' class='width40px' type='text' value='"+shop.shname+"' /></td>"));
+        tr.append($("<td>"+shop.shtotst+"</td>"));
+        tr.append($("<td><input id='updateShopBtn"+shop.shno+"' type='button' value='수정' /></td>"));
+        tr.append($("<td><input id='deleteShopBtn"+shop.shno+"' type='button' value='삭제' /></td>"));
         $('#shoplist table tbody').append(tr);
-        $('#' + 'deleteShop' + shop.shno).on('click', () => {
+        $('#updateShopBtn' + shop.shno).on('click', () => {
+            updateShop(shop.shno);
+        });        
+        $('#deleteShopBtn' + shop.shno).on('click', () => {
             deleteShop(shop.shno);
         });
     });
 };
 
 // 매장수정
+const updateShop = shno => {
+    const newShopList = getShopList().map(shop => {
+        if (shop.shno == shno) {
+            return new Shop(shop.shno, $('#shname' + shop.shno).val(), shop.shtotst);
+        } else {
+            return shop;
+        }
+    });
+    localStorage.setItem('shopList', JSON.stringify(newShopList));
+    printShopList();
+};
 
 // 매장삭제
 const deleteShop = shno => {
