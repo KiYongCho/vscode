97d845eb5dd6f6b00aee23daf57cc6dbf6facf55
realchoky <realchoky@gmail.com>
2024-07-12 15:24:06 +0900

    123

---
+++ react/HelloReact/hello/src/App.js
@@ -0,0 +1,35 @@
+//import logo from './logo.svg';
+//import './App.css';
+//import Hello from './1.Hello.js';
+//import ReactComponent from './2.ReactComponent.js';
+//import ReactProps from './3.ReactPros';
+//import ReactEvents from './4.ReactEvents.js';
+//import StringReader from './StringReader.js';
+//import NumberCounter from './NumberCounter.js';
+//import ReactUseState from './7.ReactUseState.js';
+//import GetData from './GetData.js';
+//import ReactLayout from './9.ReactLayout';
+//import ReactStyledComponent from "./10.ReactStyledComponent";
+import ReactContextAPI from './11.ReactContextAPI.js';
+
+function App() {
+  return (
+    // <>
+    //   <h1>Hello React!</h1>
+    //   <h1>헬로 리액트</h1>
+    //   <Hello />
+    // </>
+    // <ReactComponent />
+    // <ReactProps />
+    // <ReactEvents />
+    // <StringReader />
+    // <NumberCounter />
+    // <ReactUseState />
+    // <GetData />
+    // <ReactLayout />
+    // <ReactStyledComponent />
+    <ReactContextAPI />
+  );
+}
+
+export default App;
